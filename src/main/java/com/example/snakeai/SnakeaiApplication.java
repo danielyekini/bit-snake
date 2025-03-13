48643ee29003ee0f76e81136c33f5a0dbc75a420
danielyekini <feyiyek@gmail.com>
2025-03-13 19:07:33 +0000

    initial commit

---
+++ src/main/java/com/example/snakeai/SnakeaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.snakeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnakeaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/snakeai/SnakeaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snakeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
