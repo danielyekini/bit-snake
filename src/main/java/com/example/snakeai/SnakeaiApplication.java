1c4fbbad2a511d3cad1e711c92efd43ae24714be
Gitboy1232 <jj.olowu24@gmail.com>
2025-03-13 20:01:05 +0000

    hello world

--- src/main/java/com/example/snakeai/SnakeaiApplication.java
+++ src/main/java/com/example/snakeai/SnakeaiApplication.java
@@ -1,13 +1,13 @@
 package com.example.snakeai;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SnakeaiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SnakeaiApplication.class, args);
+		// SpringApplication.run(SnakeaiApplication.class, args);
+		System.out.println("Hello world");
 	}
 
 }
